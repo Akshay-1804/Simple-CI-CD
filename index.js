c46cdf31bf9ce62e6db2b5c27c46f38c52a12d3c
Akshu-18 <deoreakshay1804@gmail.com>
2024-05-06 19:37:23 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, world!  Welcome to server again!!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-console.log("Hello")
+console.log("Hello World")
