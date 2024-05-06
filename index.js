66353d924706acef23aa9c97df3d595ebad38460
Akshu-18 <deoreakshay1804@gmail.com>
2024-05-06 14:37:44 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world!  Welcome to server!!');
 });
 
 app.listen(port, () => {
